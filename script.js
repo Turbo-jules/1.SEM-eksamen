82b0a7762f395415a577ebe22a2c75693bf71d07
Julie B. Christensen <juliebovbjerg@hotmail.com>
2023-01-06 22:26:39 +0100

    Design på Portfolio side +JS hover. start på ommig

--- script.js
+++ script.js
@@ -3,3 +3,26 @@ window.addEventListener("load", sidenVises);
 function sidenVises() {
   console.log("sidenVises");
 }
+
+const portfolioMenu = document.querySelectorAll(".portfolio-menu");
+
+portfolioMenu.forEach((menuItem) => {
+  menuItem.addEventListener("mouseenter", (event) => {
+    event.target.classList.add("hovered");
+    if (event.target.nextElementSibling) {
+      event.target.nextElementSibling.classList.add("sibling-hovered");
+    }
+    if (event.target.previousElementSibling) {
+      event.target.previousElementSibling.classList.add("sibling-hovered");
+    }
+  });
+  menuItem.addEventListener("mouseleave", (event) => {
+    event.target.classList.remove("hovered");
+    if (event.target.nextElementSibling) {
+      event.target.nextElementSibling.classList.remove("sibling-hovered");
+    }
+    if (event.target.previousElementSibling) {
+      event.target.previousElementSibling.classList.remove("sibling-hovered");
+    }
+  });
+});
